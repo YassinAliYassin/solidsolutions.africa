895c62c31523dbbc883152114055109932795108
YassinAliYassin <117162331+YassinAliYassin@users.noreply.github.com>
2026-05-09 00:21:57 +0200

    Make logo transparent (use SVG with no background)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { ImgHTMLAttributes } from "react";
 export default function Logo({ className, ...props }: ImgHTMLAttributes<HTMLImageElement>) {
   return (
     <img 
-      src="/images/logo.jpg" 
+      src="/images/logo.svg" 
       alt="Solid Solutions Logo"
       className={className}
       {...props}
