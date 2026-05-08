f954ac287e6317ae64e8db4bab01e78dcc1e292a
YassinAliYassin <117162331+YassinAliYassin@users.noreply.github.com>
2026-05-09 00:27:28 +0200

    Fix logo: replace SVG with original JPG logo (img_d97874f9b331)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { ImgHTMLAttributes } from "react";
 export default function Logo({ className, ...props }: ImgHTMLAttributes<HTMLImageElement>) {
   return (
     <img 
-      src="/images/logo.svg" 
+      src="/images/logo.jpg" 
       alt="Solid Solutions Logo"
       className={className}
       {...props}
